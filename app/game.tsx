62cc7c5f81f7c173c7b493081107cb70b4f4339a
Keith Light <keith.light1986@gmail.com>
2024-02-08 09:30:30 +0000

    Update game with difficulty

--- app/game.tsx
+++ app/game.tsx
@@ -2,11 +2,11 @@
 
 import { useRef, useEffect, useState } from "react";
 let interval: any;
-let speed = 60;
 
 export function Game() {
   // States
   const [gameOver, setGameOver] = useState(false);
+  const [difficulty, setDifficulty] = useState(250); // normal difficulty
   const [score, setScore] = useState(0);
   const [highScore, setHighScore] = useState(0);
   const [velocity, setVelocity] = useState({
@@ -18,13 +18,10 @@ export function Game() {
     y: 15
   });
   const [foodPosition, setFoodPosition] = useState({
-    x: (Math.floor(Math.random() * 30) + 1),
-    y: (Math.floor(Math.random() * 30) + 1)
+    x: 0,
+    y: 0
   });
-  const [snake, setSnake] = useState<Array<any>[any]>([{
-    x: 15,
-    y: 15
-  }]);
+  const [snake, setSnake] = useState<Array<any>[any]>([snakePosition]);
 
   // Refs
   const playBoard = useRef(null);
@@ -34,33 +31,32 @@ export function Game() {
   const velocityRef = useRef(null);
 
   const changeDirection = (e: any) => {
-    let velcoityX, velcoityY;
-
     // NOTE - need to figure out why `velocity` doesnt work here, had to pass in values using `useRef`
+    let velocityXY;
+
     // Stops the snake from going the opposite direction
     if (velocityRef.current) {
       const element = velocityRef.current as HTMLElement;
-      velcoityX = Number(element.getAttribute('data-attr-x'));
-      velcoityY = Number(element.getAttribute('data-attr-y'));
+      velocityXY = JSON.parse(element.dataset.velocity as string);
     }
 
     // Changing velocity value based on key press
-    if (e.key === "ArrowUp" && velcoityY !== 1) {
+    if (e.key === "ArrowUp" && velocityXY.y !== 1) {
       setVelocity({
         x: 0,
         y: -1
       })
-    } else if (e.key === "ArrowDown" && velcoityY !== -1) {
+    } else if (e.key === "ArrowDown" && velocityXY.y !== -1) {
       setVelocity({
         x: 0,
         y: 1
       })
-    } else if (e.key === "ArrowLeft" && velcoityX !== 1) {
+    } else if (e.key === "ArrowLeft" && velocityXY.x !== 1) {
       setVelocity({
         x: -1,
         y: 0
       })
-    } else if (e.key === "ArrowRight" && velcoityX !== -1) {
+    } else if (e.key === "ArrowRight" && velocityXY.x !== -1) {
       setVelocity({
         x: 1,
         y: 0
@@ -68,24 +64,52 @@ export function Game() {
     }
   }
 
-  const updateFoodPosition = () => {
-    // Passing a random 1 - 30 value as food position
-    setFoodPosition({
-      x: (Math.floor(Math.random() * 30) + 1),
-      y: (Math.floor(Math.random() * 30) + 1)
-    })
+  const handleDirectionClick = (e: MouseEvent) => {
+    e.preventDefault();
+    const targetElement = e.currentTarget as any;
+    changeDirection({ key: targetElement.dataset.key })
   }
 
+  const updateFoodPosition = () => {
+    setFoodPosition(generateRandomPosition())
+  }
+
+  // Board size is 30x30
+  const generateRandomPosition = () => {
+    return {
+      x: (Math.floor(Math.random() * 30) + 1),
+      y: (Math.floor(Math.random() * 30) + 1)
+    }
+  }
+
+  // Clearing the timer and reloading the page on game over
   const handleGameOver = () => {
-    // Clearing the timer and reloading the page on game over
     clearInterval(interval);
     alert("Game Over! Press OK to replay...");
     location.reload();
   }
 
+  const GameBoard = () => {
+    return (
+      <div className="grid grid-cols-game-layout grid-rows-game-layout h-full" ref={playBoard}>
+        <div className={`food col-start-${foodPosition.x} row-start-${foodPosition.y}`}></div>
+        {snake.map((item: any, index: number) => (
+          <SnakeBody key={index} props={item} />
+        ))}
+      </div>
+    )
+  }
+
+  const SnakeBody = (snakeBody: any, index: number) => {
+    return (
+      <>
+        <div key={index} className={`snake-body col-start-${snakeBody.props.x} row-start-${snakeBody.props.y}`}></div>
+      </>
+    );
+  }
+
   function gameInit() {
     if (gameOver) return handleGameOver();
-
     console.log('game running');
     let newSnake = snake;
 
@@ -111,62 +135,40 @@ export function Game() {
       y: prevState.y + velocity.y,
     }));
 
+    // Shift the body parts one by one
     for (let i = newSnake.length - 1; i > 0; i--) {
       newSnake[i] = newSnake[i - 1];
     }
 
+    // Set the first body element position
     newSnake[0] = {
       x: snake[0].x + velocity.x,
       y: snake[0].y + velocity.y,
     };
 
-    // Checking if the snake's head is out of wall, if so setting gameOver to true
+    // Checking if the snake's head is out of wall, set gameOver to true
     if (snakePosition.x <= 0 || snakePosition.x > 30 || snakePosition.y <= 0 || snakePosition.y > 30) {
       setGameOver(true);
     }
 
-    // Checking if the snake head hit the body, if so set gameOver to true
+    // Checking if the snake head hit the body, set gameOver to true
     for (let i = 0; i < newSnake.length; i++) {
       if (i !== 0 && newSnake[0].y === newSnake[i].y && newSnake[0].x === newSnake[i].x) {
         setGameOver(true);
       }
     }
 
-    // Only set state once due to issues with async setState
     setSnake(newSnake);
   }
 
-  const handleDirectionClick = (e: MouseEvent) => {
-    e.preventDefault();
-    const targetElement = e.currentTarget as any;
-    changeDirection({ key: targetElement.dataset.key })
-  }
-
-  // Maybe move this into its own component
-  const GameBoard = () => {
-    return (
-      <div className="grid grid-cols-game-layout grid-rows-game-layout h-full" ref={playBoard}>
-        <div className={`food col-start-${foodPosition.x} row-start-${foodPosition.y}`}></div>
-        {snake.map((item: any, index: number) => (
-          <SnakeBody key={index} props={item} />
-        ))}
-      </div>
-    )
-  }
-
-  const SnakeBody = (snakeBody: any, index: number) => {
-    return (
-      <>
-        <div key={index} className={`snake-body col-start-${snakeBody.props.x} row-start-${snakeBody.props.y}`}></div>
-      </>
-    );
-  }
-
   useEffect(() => {
+    // Set foods starting point on load
+    setFoodPosition(generateRandomPosition());
+
     window.addEventListener('keydown', (e: KeyboardEvent) => { changeDirection(e) });
 
     return () => {
-      window.removeEventListener('keydown', (e: KeyboardEvent) => { changeDirection(e) });
+      window.removeEventListener('keydown', (e: KeyboardEvent) => { });
     }
   }, []);
 
@@ -181,7 +183,7 @@ export function Game() {
   useEffect(() => {
     interval = setInterval(() => {
       gameInit();
-    }, speed);
+    }, difficulty);
 
     return () => {
       clearInterval(interval);
@@ -190,10 +192,20 @@ export function Game() {
 
   return (
     <div className="aspect-square max-w-6xl min-h-96 rounded bg-elephant flex flex-col">
-      <div ref={velocityRef} data-attr-x={velocity.x} data-attr-y={velocity.y} />
+      <div ref={velocityRef} data-velocity={JSON.stringify(velocity)} />
       <div className="bg-midnight-express flex justify-between p-4">
-        <span className="">Score: <span ref={scoreElement}>{score}</span></span>
-        <span className="">High Score: <span ref={highScoreElement}>{highScore}</span></span>
+        <div className="">
+          <select
+            className="bg-midnight-express"
+            onChange={e => setDifficulty(Number(e.target.value))}
+          >
+            <option value={500}>Easy</option>
+            <option value={250} selected>Normal</option>
+            <option value={120}>Hard</option>
+          </select>
+        </div>
+        <div className="">Score: <span ref={scoreElement}>{score}</span></div>
+        <div className="">High Score: <span ref={highScoreElement}>{highScore}</span></div>
       </div>
       <GameBoard />
       <div className="flex md:hidden" ref={controls}>
@@ -202,28 +214,28 @@ export function Game() {
           className="flex-1 py-4 px-2 text-center"
           onClick={(e: any) => handleDirectionClick(e)}
         >
-          <span>Left</span>
+          <span>&#8592;</span>
         </button>
         <button
           data-key="ArrowUp"
           className="flex-1 py-4 px-2 text-center"
           onClick={(e: any) => handleDirectionClick(e)}
         >
-          <span>Up</span>
+          <span>&#8593;</span>
         </button>
         <button
           data-key="ArrowRight"
           className="flex-1 py-4 px-2 text-center"
           onClick={(e: any) => handleDirectionClick(e)}
         >
-          <span>Right</span>
+          <span>&#8594;</span>
         </button>
         <button
           data-key="ArrowDown"
           className="flex-1 py-4 px-2 text-center"
           onClick={(e: any) => handleDirectionClick(e)}
         >
-          <span>Down</span>
+          <span>&#8595;</span>
         </button>
       </div>
     </div>
